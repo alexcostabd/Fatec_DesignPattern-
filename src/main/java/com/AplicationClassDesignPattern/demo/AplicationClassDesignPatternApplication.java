49379b67199a3db1b6827a5fcf7466145125dfeb
alexcostabd <alex.costa19@fatec.sp.gov.br>
2022-02-23 19:59:58 -0300

    Initial Commit

---
+++ src/main/java/com/AplicationClassDesignPattern/demo/AplicationClassDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.AplicationClassDesignPattern.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicationClassDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicationClassDesignPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AplicationClassDesignPattern/demo/AplicationClassDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AplicationClassDesignPattern.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicationClassDesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
